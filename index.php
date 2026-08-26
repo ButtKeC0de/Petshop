ed7063395c0e470c1b66dd3663183d462cb66089
ButtKeC0de <carlos_buttke@estudante.sesisenai.org.br>
2026-08-26 07:42:12 -0300

    UP - INDEX

--- index.php
+++ index.php
@@ -13,41 +13,56 @@
         <h2>Sua loja de animais favorita !</h2>
     </header>
 
-    <body>
-
-    <div class="cliente">
-        <form action="processar_cadastro.php" method="POST">
+    <main>
+        
+        <div class="container1">
             <h1>Cadastro do cliente </h1>
-            <label for="nome">Nome:</label>
-            <input type="text" id="nome" name="nome" required>
-            <br><br>
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required>
-            <br><br>
-            <label for="telefone">Telefone:</label>
-            <input type="tel" id="telefone" name="telefone" required>
-            <br><br>
-        </div>
-        <div class="pet"></div>
-        <h1>Cadastre o AUmigo </h1>
-        <label for="nome_au">Nome do AUmigo:</label>
-            <input type="text" id="nome_au" name="nome_au" required>
-            <br><br>
-            <label for="tipo">Tipo de animal:</label>
-            <input type="text" id="tipo" name="tipo" required>
-            <br><br>
-            <label for="raca">Raça do Animal:</label>
-            <input type="text" id="raca" name="raca" required>
-            <br><br>
-            <label for="idade">Idade:</label>
-            <input type="number" id="idade" name="idade" required>
-            <br><br>
-            <input type="submit" value="Cadastrar ">
+            <form action="processar_cadastro.php" method="POST">
+                <label for="nome">Nome:</label>
+                <input type="text" id="nome" name="nome" required>
+                <br><br>
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required>
+                <br><br>
+                <label for="telefone">Telefone:</label>
+                <input type="tel" id="telefone" name="telefone" required>
+                <br><br>
+                <input type="submit" value="Cadastrar">
+            </div>
+         
+        <div class="container1">
+            <h1>Cadastre o AUmigo </h1>
+            <form action="processar_cadastro_au.php" method="POST">
+                <label for="nome_au">Nome do AUmigo:</label>
+                <input type="text" id="nome_au" name="nome_au" required>
+                <br><br>
+                <label for="tipo">Tipo de animal:</label>
+                <input type="text" id="tipo" name="tipo" required>
+                <br><br>
+                <label for="raca">Raça do Animal:</label>
+                <input type="text" id="raca" name="raca" required>
+                <br><br>
+                <label for="idade">Idade:</label>
+                <input type="number" id="idade" name="idade" required>
+                <br><br>
+                <input type="submit" value="Cadastrar AUmigo">
+            </div>
+                
+                
+            </main>
+            
 
-
-
-    </body>
-
-</body>
-
+        </body>
+        
 </html>
+
+<?php
+
+
+
+
+
+
+
+
+?>
